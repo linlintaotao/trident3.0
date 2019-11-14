892d6f133b8afb3f888d42d9d58d6bbbf0ae24e0
Chey <cheyu856@gmail.com>
2019-11-14 21:58:06 +0800

    :construction: first beta version

--- FmiSerialTools.py
+++ FmiSerialTools.py
@@ -12,10 +12,11 @@ from datetime import datetime
 from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 from PyQt5.QtGui import QTextCursor
-from PyQt5.QtCore import QTimer, QThread
+from PyQt5.QtCore import QTimer
 from PyQt5.QtNetwork import QTcpSocket
 from gui.form import Ui_Form
-from streams import NtripClient
+
+# from streams import NtripClient
 
 OPEN = 'Open'
 CLOSE = 'Close'
@@ -26,32 +27,13 @@ DISCONNECT = 'Disconnect'
 def gettstr():
     return datetime.now().strftime("%Y%m%d%H%M%S")
 
-
-class NtripThread(QThread):
-    def __init__(self, ntrip):
-        super(NtripThread, self).__init__()
-        self.ntrip = ntrip
-
-    def run(self):
-        self.ntrip.readData()
-
-    def __del__(self):
-        self.wait()
-
-
-# TODO: serial and ntrip status info display
-class MyMainWindow(QMainWindow, Ui_Form):
+class NtripSerialTool(QMainWindow, Ui_Form):
+    """
+    Ntrip serial tool for testing FMI P20 comb board
+    """
     def __init__(self, parent=None):
-        """
-
-        :param parent:
-        """
-        super(MyMainWindow, self).__init__(parent)
+        super(NtripSerialTool, self).__init__(parent)
         self.fh = None
-        self.ntrip = None
-        self.CorsThread = None
-
-        self.found_header = False
         self.tmpggadata = ''
         self.tmprefdata = ''
         self.getmnt = b''
@@ -60,27 +42,25 @@ class MyMainWindow(QMainWindow, Ui_Form):
         self.stxbs = 0
         self.nrxbs = 0
         self.ntxbs = 0
+        self.val = 0
         self.setupUi(self)
         self.CreateItems()
         self.CreateSignalSlot()
 
     def CreateItems(self):
         """
-
+        create serial, socket, timer instance
         :return:
         """
         self.com = QSerialPort()
         self.sock = QTcpSocket()
-        # self.NtripTimer = QTimer(self)
-        # self.NtripTimer.timeout.connect(self.ImportCors)
-        # self.NtripTimer.start(100)
         self.SendGGATimer = QTimer(self)
         self.SendGGATimer.timeout.connect(self.SendGGA)
         self.SendGGATimer.start(10000)
 
     def CreateSignalSlot(self):
         """
-
+        connect signal with slots
         :return:
         """
         self.pushButton_open.clicked.connect(self.SOpenBtClick)
@@ -93,15 +73,13 @@ class MyMainWindow(QMainWindow, Ui_Form):
 
         self.com.readyRead.connect(self.SerialRecvData)
         self.sock.connected.connect(self.SockConnect)
-        self.sock.disconnected.connect(self.SockDisconnect)
         self.sock.readyRead.connect(self.SockRecv)
 
         self.textEdit_recv.textChanged.connect(self.TextRecvChanged)
 
-    # refresh serial
     def SRefreshBtClick(self):
         """
-
+        refresh current serial port list
         :return:
         """
         self.cbsport.clear()
@@ -113,34 +91,10 @@ class MyMainWindow(QMainWindow, Ui_Form):
                 self.cbsport.addItem(info.portName())
                 com.close()
 
-    def SetSerialPara(self, enable):
-        """
-
-        :param enable:
-        :return:
-        """
-        self.cbsport.setEnabled(enable)
-        self.cbsbaud.setEnabled(enable)
-        self.cbsdata.setEnabled(enable)
-        self.cbsstop.setEnabled(enable)
-        self.cbsparity.setEnabled(enable)
-
-    def SetNtripPara(self, enable):
-        """
-
-        :param enable:
-        :return:
-        """
-        self.comboBox_caster.setEnabled(enable)
-        self.comboBox_port.setEnabled(enable)
-        self.comboBox_mount.setEnabled(enable)
-        self.lineEdit_user.setEnabled(enable)
-        self.lineEdit_pwd.setEnabled(enable)
-
     # serial configuration parameters
     def SOpenBtClick(self):
         """
-
+        serial open button clicked handling
         :return:
         """
         if self.pushButton_open.text() == OPEN:
@@ -152,25 +106,28 @@ class MyMainWindow(QMainWindow, Ui_Form):
             try:
                 self.com.open(QSerialPort.ReadWrite)
             except Exception as e:
-                QMessageBox.critical(self, 'Critical', f"{sys._getframe().f_code.co_name}, {e}")
+                print(f"{sys._getframe().f_code.co_name}, {e}")
             else:
                 self.SetSerialPara(False)
-                # revert push button text label
                 self.pushButton_open.setText(CLOSE)
+                self.pushButton_refresh.setEnabled(False)
         elif self.pushButton_open.text() == CLOSE:
             self.com.close()
             self.SetSerialPara(True)
             self.pushButton_open.setText(OPEN)
+            self.pushButton_refresh.setEnabled(True)
 
-    # serial received data display
-    # TODO: gga info textEdit display
     def SerialRecvData(self):
         """
-
+        serial port reading
         :return:
         """
         try:
             data = bytes(self.com.readAll())
+        except Exception as e:
+            print(f"{sys._getframe().f_code.co_name}, {e}")
+            # self.com.close()
+        else:
             self.curgga = data
             self.srxbs += len(data)
             data = data.decode("utf-8", "ignore")
@@ -181,17 +138,16 @@ class MyMainWindow(QMainWindow, Ui_Form):
                     self.fh.write(data)
             else:
                 self.fh = None
-        except Exception as e:
-            print(f"{sys._getframe().f_code.co_name}, {e}")
-            # self.com.close()
-        else:
             self.textEdit_recv.insertPlainText(data)
             self.lineEdit_srx.setText(str(self.srxbs))
-            self.lineEdit_stx.setText(str(self.stxbs))
             self.DisplayGGA(data)
 
-
     def DisplayGGA(self, data):
+        """
+        display gga string
+        :param data: gga string
+        :return:
+        """
         if len(data.strip("\r\n").split(",")) <= 14:
             self.tmpggadata += data
         else:
@@ -222,6 +178,11 @@ class MyMainWindow(QMainWindow, Ui_Form):
             self.tmpggadata = ""
 
     def DisplayRef(self, data):
+        """
+        display geref string
+        :param data: ref string
+        :return:
+        """
         if len(data.strip("\r\n").split(",")) <= 4:
             self.tmprefdata += data
         else:
@@ -236,12 +197,22 @@ class MyMainWindow(QMainWindow, Ui_Form):
             self.lineEdit_baseid.setText(bid)
             self.tmprefdata = ""
 
+    def SetSerialPara(self, enable):
+        self.cbsport.setEnabled(enable)
+        self.cbsbaud.setEnabled(enable)
+        self.cbsdata.setEnabled(enable)
+        self.cbsstop.setEnabled(enable)
+        self.cbsparity.setEnabled(enable)
+
+    def SetNtripPara(self, enable):
+        self.comboBox_caster.setEnabled(enable)
+        self.comboBox_port.setEnabled(enable)
+        self.comboBox_mount.setEnabled(enable)
+        self.lineEdit_user.setEnabled(enable)
+        self.lineEdit_pwd.setEnabled(enable)
+
     # get ntrip caster params, invoke NtripClient thread
     def NConnBtClick(self):
-        """
-
-        :return:
-        """
         if self.pushButton_conn.text() == CONNECT:
             if self.lineEdit_user.text() == '' or self.lineEdit_pwd.text() == '':
                 caster, port, mount = 'ntrips.feymani.cn', 2102, 'Obs'
@@ -252,37 +223,21 @@ class MyMainWindow(QMainWindow, Ui_Form):
                 mount = self.comboBox_mount.currentText()
                 user = self.lineEdit_user.text()
                 passwd = self.lineEdit_pwd.text()
-            # create & start ntrip client thread
-            if self.ntrip is None:
-                try:
-                    user = b64encode((user+":"+passwd).encode('utf-8')).decode()
-                    self.setMountPointString(mount, user)
-                    self.NtripConnect(caster, port, mount, user, passwd)
-                    self.sock.connectToHost(caster, port)
-                    if not self.sock.waitForConnected(5000):
-                        msg = self.sock.errorString()
-                        print(f"{msg}")
 
-                    # self.ntrip = NtripClient.NtripClient(caster=caster, port=port, mountpoint=mount, user=user,
-                    #                                      passwd=passwd)
-                    # self.CorsThread = NtripThread(self.ntrip)
-                    # self.CorsThread.start()
-                    # self.SetNtripPara(False)
-                    self.pushButton_conn.setText(DISCONNECT)
-                except Exception as e:
-                    print(f"{e}")
+            if not self.sock.isOpen():
+                self.sock.open(QTcpSocket.ReadWrite)
+            # get mount point and try to connect caster
+            user = b64encode((user + ":" + passwd).encode('utf-8')).decode()
+            self.setMountPointString(mount, user)
+            self.NtripConnect(caster, port)
+            self.SetNtripPara(False)
+            self.pushButton_conn.setText(DISCONNECT)
+
         elif self.pushButton_conn.text() == DISCONNECT:
-            # terminate ntrip client thread
             self._TerminateNtrip()
 
-    # send AT command
     def AtCmdBtClick(self):
-        """
-        while AT command button clicked ntrip data must not imported into serial
-        :return:
-        """
-        # if self.ntrip is not None and self.CorsThread is not None:
-        if not self.sock.disconnected():
+        if self.sock.isOpen():
             self._TerminateNtrip()
 
         cmd = self.cbatcmd.currentText() + "\r\n"
@@ -291,33 +246,13 @@ class MyMainWindow(QMainWindow, Ui_Form):
         self.com.write(cmd.encode("utf-8", "ignore"))
         print(f"AT cmd {cmd}")
 
-    # # import cors rtcm data into serial
-    # def ImportCors(self):
-    #     """
-    #
-    #     :return:
-    #     """
-    #     if self.ntrip is not None and self.CorsThread is not None:
-    #         if self.com.isOpen():
-    #             try:
-    #                 data = self.ntrip.data.pop()
-    #                 self.com.write(data)
-    #                 self.stxbs += len(data)
-    #                 self.progressBar.setValue(self.stxbs % 100)
-    #             except IndexError as e:
-    #                 pass
-    #         else:
-    #             print(f"Open serial first please")
-    #     # else:
-    #     #     print(f"No valid ntrip or cors thread instance")
-
     def setMountPointString(self, mnt, user):
         mountPointString = "GET /%s HTTP/1.1\r\nUser-Agent: %s\r\nAuthorization: Basic %s\r\n" % (
-                             mnt, "NTRIP FMIPythonClient/1.0", user)
+            mnt, "NTRIP FMIPythonClient/1.0", user)
         mountPointString += "\r\n"
         self.getmnt = mountPointString.encode('utf-8')
 
-    def NtripConnect(self, caster, port, mnt, user, passwd):
+    def NtripConnect(self, caster, port):
         self.sock.connectToHost(caster, port)
         if not self.sock.waitForConnected(5000):
             msg = self.sock.errorString()
@@ -325,56 +260,44 @@ class MyMainWindow(QMainWindow, Ui_Form):
             QMessageBox.critical(self, "Error", msg)
             return
 
-    # sock connect
+    # sock connect, write get mount point request
     def SockConnect(self):
         try:
             self.sock.write(self.getmnt)
         except Exception as e:
             print(f"{e}")
 
-
-    def SockDisconnect(self):
-        self.sock.flush()
-        self.sock.close()
-
+    # socket receive data
     def SockRecv(self):
         rtcm = self.sock.readAll()
         if self.com.isOpen():
             try:
                 self.com.write(rtcm)
+            except Exception as e:
+                print(f"{e}")
+            else:
                 self.stxbs += len(rtcm)
-                self.progressBar.setValue(self.stxbs % 100)
-            except IndexError as e:
-                pass
+                self.val += 5
+                self.val = 0 if self.val > 100 else self.val
+                self.progressBar.setValue(self.val)
+                self.lineEdit_stx.setText(str(self.stxbs))
         else:
             print(f"Open serial first please")
 
-
-    def SockTransit(self):
-        pass
-
     # send gga to ntrip server
     def SendGGA(self):
-        if self.ntrip is not None and self.CorsThread is not None:
+        if self.sock.isOpen():
             if self.checkBox_sendgga.isChecked():
                 if self.curgga.decode("utf-8").startswith("$GNGGA"):
                     print(f"Transit {self.curgga} to ntrip")
-                    self.ntrip.sendGGA2Ntrip(self.curgga)
+                    self.sock.write(bytes(self.curgga))
 
     # clear serial received data
     def SRecvClearBtClick(self):
-        """
-
-        :return:
-        """
         self.textEdit_recv.clear()
 
     # text cursor at end
     def TextRecvChanged(self):
-        """
-
-        :return:
-        """
         self.textEdit_recv.moveCursor(QTextCursor.End)
 
     # close window
@@ -383,7 +306,7 @@ class MyMainWindow(QMainWindow, Ui_Form):
             self.com.flush()
             self.com.close()
 
-        if self.ntrip is not None:
+        if self.sock.isOpen():
             self._TerminateNtrip()
 
         if self.fh is not None:
@@ -398,17 +321,14 @@ class MyMainWindow(QMainWindow, Ui_Form):
             self.SetSerialPara(True)
             self.pushButton_open.setText(OPEN)
 
-        if self.ntrip is not None:
+        if self.sock.isOpen():
             self._TerminateNtrip()
 
     # terminate ntrip connection
     def _TerminateNtrip(self):
-        """"""
-        # self.ntrip.terminate()
-        # self.ntrip.data.clear()
-        self.sock.disconnect()
+        self.sock.flush()
+        self.sock.close()
         self.com.flush()
-        # self.ntrip, self.CorsThread = None, None
         self.SetNtripPara(True)
         self.pushButton_conn.setText(CONNECT)
         print(f"Ntrip client terminating...")
@@ -416,6 +336,6 @@ class MyMainWindow(QMainWindow, Ui_Form):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    myWin = MyMainWindow()
+    myWin = NtripSerialTool()
     myWin.show()
     sys.exit(app.exec_())
