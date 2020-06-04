f8e76f722b685b24f841a5b95630ea96196ee45b
linlintaotao <a574671110@163.com>
2020-06-04 17:30:30 +0800

    add AT+update_mode_H by lt

--- gui/form.py
+++ gui/form.py
@@ -9,6 +9,8 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+BUILD_FOR_FMI = False
+
 
 class Ui_widget(object):
     def setupUi(self, widget):
@@ -644,19 +646,21 @@ class Ui_widget(object):
         self.cbatcmd.setItemText(17, _translate("widget", "AT+ACTIVATE_KEY="))
         self.cbatcmd.setItemText(18, _translate("widget", "AT+ALT_PARA=-9.1,37,1"))
         self.cbatcmd.setItemText(19, _translate("widget", "AT+LEVER_ARM=0.01,-0.027,-0.1"))
-        self.cbatcmd.setItemText(20, _translate("widget", "$JASC,"))
-        self.cbatcmd.setItemText(21, _translate("widget", "$JATT,"))
-        self.cbatcmd.setItemText(22, _translate("widget", "$JBAUD,"))
-        self.cbatcmd.setItemText(23, _translate("widget", "$JBIN,"))
-        self.cbatcmd.setItemText(24, _translate("widget", "$JBOOT,"))
-        self.cbatcmd.setItemText(25, _translate("widget", "$JDIFF,"))
-        self.cbatcmd.setItemText(26, _translate("widget", "$JNMEA,"))
-        self.cbatcmd.setItemText(27, _translate("widget", "$JOFF,"))
-        self.cbatcmd.setItemText(28, _translate("widget", "$JPOS,"))
-        self.cbatcmd.setItemText(29, _translate("widget", "$JQUERY,"))
-        self.cbatcmd.setItemText(30, _translate("widget", "$JRTCM3,"))
-        self.cbatcmd.setItemText(31, _translate("widget", "$JRTK,"))
-        self.cbatcmd.setItemText(32, _translate("widget", "$JSHOW,"))
+        self.cbatcmd.setItemText(20, _translate("widget", "AT+AUTONOMOUS=ENABLE"))
+        if BUILD_FOR_FMI:
+            self.cbatcmd.setItemText(21, _translate("widget", "$JASC,"))
+            self.cbatcmd.setItemText(22, _translate("widget", "$JATT,"))
+            self.cbatcmd.setItemText(23, _translate("widget", "$JBAUD,"))
+            self.cbatcmd.setItemText(24, _translate("widget", "$JBIN,"))
+            self.cbatcmd.setItemText(25, _translate("widget", "$JBOOT,"))
+            self.cbatcmd.setItemText(26, _translate("widget", "$JDIFF,"))
+            self.cbatcmd.setItemText(27, _translate("widget", "$JNMEA,"))
+            self.cbatcmd.setItemText(28, _translate("widget", "$JOFF,"))
+            self.cbatcmd.setItemText(29, _translate("widget", "$JPOS,"))
+            self.cbatcmd.setItemText(30, _translate("widget", "$JQUERY,"))
+            self.cbatcmd.setItemText(31, _translate("widget", "$JRTCM3,"))
+            self.cbatcmd.setItemText(32, _translate("widget", "$JRTK,"))
+            self.cbatcmd.setItemText(33, _translate("widget", "$JSHOW,"))
         self.pushButton_atcmd.setText(_translate("widget", "Send"))
         self.open_file.setText(_translate("widget", "File"))
         self.trans_file.setText(_translate("widget", "Trans"))
